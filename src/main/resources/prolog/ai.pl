3b6c11c6164b501f8394783907e037c76e527d21
Or Yaacov <oryaacov@live.com>
2020-10-21 20:51:04 +0300

    fixed the alphabet algorithm

--- src/main/resources/prolog/ai.pl
+++ src/main/resources/prolog/ai.pl
@@ -10,6 +10,7 @@ moveHeuristic(CurrentBoard,NextBoard,Res):-
     earnerHeuristic(CurrentBoard,NextBoard,EarnerRes),
     weakerHeuristic(CurrentBoard,NextBoard,WeakerRes),
     escaperHeuristic(NextBoard,EscaperRes),
+
     Res is (*(EarnerRes,1))+(*(WeakerRes,1))+(*(EscaperRes,1)).
 
 % the earner heuristic focus on the current player Lala's seed count (aka player score)
@@ -24,8 +25,8 @@ earnerHeuristic(CurrentBoard,NextBoard,Res):-
 % and reducing is seeds amount.
 % works by calculating the diff between his prev and current seed count.
 weakerHeuristic(CurrentBoard,NextBoard,Res):-
-    getCurrentPlayerMarblesCount(CurrentBoard,CurrentCount),
-    getOtherPlayerMarblesCount(NextBoard,NextCountCount),
+    countOtherPlayerHoleSeeds(CurrentBoard,CurrentCount),
+    countOtherPlayerHoleSeeds(NextBoard,NextCountCount),
     Temp is CurrentCount-NextCountCount,
     (Temp>0,Res is Temp;
     Res is 0).
@@ -44,6 +45,11 @@ countEmptyHoles([Hole|Board],Index,Res):-
     (Hole =:= 0, Res is Temp+1;
     Res is Temp).
 
+countOtherPlayerHoleSeeds(Board,Res):-
+    Board = [_,_,_,_,_,_,_,H1,H2,H3,H4,H5,H6,_],
+    Res is H1+H2+H3+H4+H5+H6.
+
+
 
 %the alpha beta algorithm
 %entry when depth is bigger than zero
@@ -51,34 +57,63 @@ countEmptyHoles([Hole|Board],Index,Res):-
 %Board - is the current game state (the current move)
 %Alpha/Beta - the algoritm's alpha/beta value
 %BestMove,Value - the best move and it's value which found by the alphabeta algorithm (the result).
-alpha_beta( Depth,_, Board, Alpha, Beta, Move, Value ) :-
-    possibleMoves(Board, Moves),
-    not(isEmpty(Moves)), !,
-    NewDepth is Depth - 1,
-    NewAlpha is -Beta,
-    NewBeta is -Alpha,
-    evaluate_and_choose( Moves, Board, NewDepth, NewAlpha, NewBeta, nil, ( Move, Value )).
+%first alpha beta accures when a player as more than one turn we treat it as a "single move"
+%and there for not changing depth,alpha and beta values.
+alphabeta(Ancestor,Board, _, _, _, Val, 0,CurrentPlayer) :- % max depth of search recieved
+  moveHeuristic(Ancestor, Board, CurrentPlayer,Val),!.
 
-alpha_beta( 0,Ancestor, Board, _, _, _, Value ) :-
-  moveHeuristic(Ancestor, Board, Value).
+alphabeta(_,Board, Alpha, Beta, GoodPos, Val, Depth,CurrentPlayer) :-
+   Depth > 0,
+   possibleMoves(Board, PosList), !,
+   boundedbest(Board,PosList, Alpha, Beta, GoodPos, Val,Depth,CurrentPlayer).
 
+%if there are no possible moves
+alphabeta(Ancestor,Board, _, _, _, Val, Depth,CurrentPlayer) :-
+  Depth > 0,
+  moveHeuristic(Ancestor, Board,CurrentPlayer, Val).
 
-evaluate_and_choose([ Move | Moves ], Board, Depth, Alpha, Beta, Move1, BestMove ) :-
-    move( Move, Board, NewBoard ),
-    alpha_beta(Depth, Board, NewBoard, Alpha, Beta, Move1, Value ),
-    NewValue is -Value,
-    cutoff( Move, NewValue, Depth, Alpha, Beta, Moves, Board, Move1, BestMove ).
+changePlayer(ChangePlayer,CurrentPlayer,NextPlayer):-
+   ChangePlayer is 1,
+   Temp is 1-CurrentPlayer,
+   NextPlayer is abs(Temp).
 
-evaluate_and_choose( [], _, _, _, _, Move, ( Move, _ )).
+boundedbest(Ancestor,[Move | MoveList], Alpha, Beta, GoodPos, GoodVal,Depth,CurrentPlayer):-
+   Depth>0,
+   %print('player:'),print(CurrentPlayer),nl,
+   executeMove( Move, Ancestor, NewBoard, ChangePlayer),!,
+   (changePlayer(ChangePlayer,CurrentPlayer,NextPlayer),
+   Depth1 is Depth - 1,
+   alphabeta(Ancestor,NewBoard, Alpha, Beta, _, Val,Depth1,NextPlayer),
+  % print('(change player)before val:'),print(Val),nl,print('alpha:'),print(Alpha),nl,print('beta'),print(Beta),nl,
+   goodenough(Ancestor,MoveList, Alpha, Beta, Move-NewBoard, Val, GoodPos, GoodVal,Depth,CurrentPlayer,NextPlayer);
+   %print('(change player)after val:'),print(Val),nl,print('alpha:'),print(Alpha),nl,print('beta'),print(Beta),nl,
+   alphabeta(Ancestor,NewBoard, Alpha, Beta, _, Val,Depth,CurrentPlayer),!,
+   %print('(same player)before val:'),print(Val),nl,print('alpha:'),print(Alpha),nl,print('beta'),print(Beta),nl,
+   goodenough(Ancestor,MoveList, Alpha, Beta, Move-NewBoard, Val, GoodPos, GoodVal,Depth,CurrentPlayer,NextPlayer)).
+   %print('(same player)after val:'),print(Val),nl,print('alpha:'),print(Alpha),nl,print('beta'),print(Beta),nl.
 
-cutoff(Move, Value, _, _, Beta, _, _, _, (Move, Value)) :-
-    Value >= Beta.
+goodenough(_,[],_,_,Move-NewBoard, Val, Move-NewBoard, Val,_,_,_):-!.
 
-cutoff(Move, Value, Depth, Alpha, Beta, Moves, Position, _, BestMove) :-
-    Alpha < Value,
-    Value < Beta,
-    evaluate_and_choose(Moves, Position, Depth, Value, Beta, Move, BestMove).
+goodenough(_,_, Alpha, Beta, Move-NewBoard, Val, Move-NewBoard, Val,_,CurrentPlayer,_) :-
+  CurrentPlayer is 1, Val > Beta, !;
+  CurrentPlayer is 0, Val < Alpha, !.
 
-cutoff(_, Value, Depth, Alpha, Beta, Moves, Position, PotMove, BestMove) :-
-    Value =< Alpha,
-    evaluate_and_choose(Moves, Position, Depth, Alpha, Beta, PotMove, BestMove).
+goodenough(Ancestor,PosList, Alpha, Beta, Move-NewBoard, Val, GoodPos, GoodVal,Depth,CurrentPlayer,_) :-
+   newbounds( Alpha, Beta, Move, Val, NewAlpha, NewBeta,CurrentPlayer),
+   boundedbest(Ancestor,PosList, NewAlpha, NewBeta, Pos1, Val1,Depth,CurrentPlayer),
+   betterof( Move-NewBoard, Val, Pos1, Val1, GoodPos, GoodVal,CurrentPlayer),!.
+
+newbounds( Alpha, Beta, _, Val, Val, Beta,CurrentPlayer)  :-
+CurrentPlayer is 1, Val > Alpha,!.
+
+newbounds( Alpha, Beta,_, Val, Alpha, Val,CurrentPlayer)  :-
+CurrentPlayer is 0, Val < Beta, !.
+
+newbounds( Alpha, Beta, _, _, Alpha, Beta,_).
+
+betterof( Move-NewBoard, Val, _, Val1, Move-NewBoard, Val,CurrentPlayer):-
+CurrentPlayer is 1, Val > Val1, !
+;
+CurrentPlayer is 0, Val < Val1, !.
+
+betterof( _, _, Move-NewBoard, Val, Move-NewBoard, Val,_):-!.                       % otherwise Pos 1 better
